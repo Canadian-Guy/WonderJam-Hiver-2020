018039749776d1f01053d6f743ad92cd15b18e97
CanadianGuy <jason_589@live.ca>
2020-02-28 18:37:02 -0500

    Smooth score increment
    Le score s'update de facon smooth avec une coroutine qui run dans le background.

--- Assets/Code/ScoreHandler.cs
+++ Assets/Code/ScoreHandler.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class ScoreHandler : MonoBehaviour
@@ -7,12 +8,18 @@ public class ScoreHandler : MonoBehaviour
     public int Score = 0;
     [Tooltip("Text component that displays the score.")]
     public TMP_Text ScoreText;
+    [Tooltip("Score increment tick speed, in seconds")]
+    public float Speed = 0;
+    [Tooltip("Score tick increments")]
+    public int Increment = 0;
+    private bool IsItTheEndOfTimes = false;
 
     // Start is called before the first frame update
     void Start()
     {
         Score = 0;
-        ScoreText.text = Score.ToString() + "k";
+        ScoreText.text = Score.ToString();
+        StartCoroutine(UpdateScore());
     }
 
     // Update is called once per frame
@@ -24,13 +31,38 @@ public class ScoreHandler : MonoBehaviour
     public void AddScore(int p_score, int p_combo = 1)
     {
         Score += (p_score * p_combo);
-        ScoreText.text = Score.ToString() + "k";
+        //ScoreText.text = Score.ToString();
     }
 
     public void Reset()
     {
+        //Reset le score
         Score = 0;
-        ScoreText.text = "0k";
+        //Reset le UI
+        ScoreText.text = "0";
+        //Reset la coroutine
+        StopCoroutine(UpdateScore());
+        StartCoroutine(UpdateScore());
+    }
+
+    IEnumerator UpdateScore()
+    {
+        //As long as it is not the end of times...
+        while(!IsItTheEndOfTimes) {
+            if(Score.ToString() != ScoreText.text)
+            {
+                //tmp score est le score affiché
+                int m_tmpScore = int.Parse(ScoreText.text);
+                m_tmpScore += Increment;
+                if(m_tmpScore > Score)
+                {
+                    //Si on depasse le score, on a merdé et on devient égal au score.
+                    m_tmpScore = Score;
+                }
+                ScoreText.text = m_tmpScore.ToString();
+            }
+            yield return new WaitForSeconds(Speed);
+        }
     }
 
     public void DEBUG_AddScoreCombo0()
