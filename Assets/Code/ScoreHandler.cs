9d09b52fa9044c92d74e0ae875954defbd447524
CanadianGuy <jason_589@live.ca>
2020-02-28 17:42:19 -0500

    ScoreHandler V1
    Ajout d'un script qui display un score dans un textmeshpro. il a une fonction publique qui prend deux int, un pour le score à ajouter et un pour le combo à appliquer. Deux fonctions temporaires pour tester avec des boutons.

---
+++ Assets/Code/ScoreHandler.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreHandler : MonoBehaviour
+{
+    [Tooltip("This is the current score!")]
+    public int Score = 0;
+    [Tooltip("Text component that displays the score.")]
+    public TMP_Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+        ScoreText.text = Score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int p_score, int p_combo = 1)
+    {
+        Score += (p_score * p_combo);
+        ScoreText.text = Score.ToString();
+    }
+
+    public void DEBUG_AddScoreCombo0()
+    {
+        AddScore(5);
+    }
+
+    public void DEBUG_AddScoreCombo5()
+    {
+        AddScore(5, 5);
+    }
+}
