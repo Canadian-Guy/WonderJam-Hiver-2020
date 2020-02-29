6dc683b239e52a19fbe3d816cd24c7cce08fdbb5
Etienne <etienneemond97@hotmail.com>
2020-02-29 14:45:45 -0500

    highlight

--- Assets/Code/Components/InputHandler.cs
+++ Assets/Code/Components/InputHandler.cs
@@ -23,6 +23,9 @@ public class InputHandler : MonoBehaviour
     public AudioClip InvalidSound;
 
     private AudioSource audioSource;
+
+    public Color highlightColor;
+
     private void Awake()
     {
         int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
@@ -39,10 +42,34 @@ public class InputHandler : MonoBehaviour
         Clear();
     }
 
+    private void Start()
+    {
+        StartCoroutine(UpdateHighlight());
+    }
+
     public void PlayKeyStroke()
     {
         audioSource.PlayOneShot(KeystrokeSound);
     }
+
+    public void HighlightWords()
+    {
+        for (int i = activeWordSet.Count() - 1; i >= 0; i--)
+        {
+            FallingWord fw = activeWordSet._items[i];
+
+            if (inputField.text != "" && fw.Wrapper.Word.Text.StartsWith(inputField.text))
+            {
+                string part1 = inputField.text;
+                string part2 = fw.Wrapper.Word.Text.Substring(part1.Length, fw.Wrapper.Word.Text.Length - part1.Length);
+
+                fw.Text.text = "<color=#" + ColorUtility.ToHtmlStringRGB(highlightColor) + ">" + part1 + "</color>" + part2;
+            }
+            else
+                fw.Text.text = fw.Wrapper.Word.Text;
+        }
+    }
+
 
     public void ConfirmInput()
     {
@@ -90,5 +117,14 @@ public class InputHandler : MonoBehaviour
     private void Clear()
     {
         inputField.text = "";
+    }
+
+    private IEnumerator UpdateHighlight()
+    {
+        while (true)
+        {
+            HighlightWords();
+            yield return new WaitForSeconds(0.01f);
+        }
     }
 }
