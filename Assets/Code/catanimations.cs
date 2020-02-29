9af52925a48b6b0a3f8699edacc9472b719eb3e6
Nath <nathanaeltremblay19@gmail.com>
2020-02-29 03:42:37 -0500

    bit of uis, and some code behind it
    ui and firends

---
+++ Assets/Code/catanimations.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catanimations : MonoBehaviour
+{
+    public Animator m_animator;
+
+    public Timer m_timer;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        m_animator.SetBool("typing", false);//only for RN
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_timer.Night == true)
+        {
+            m_animator.SetBool("night", true);
+        }
+        else
+        {
+            m_animator.SetBool("night", false);
+        }
+    }
+}
